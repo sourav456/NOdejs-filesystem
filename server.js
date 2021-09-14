a1f6e90c153e9363555b5e82180afe41f68b56c0
Sourav Biswakarma <sourav.biswa23@gmail.com>
2021-09-14 19:49:22 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const moment = require("moment");
+const fs = require("fs");
+const currentFolder = __dirname;
+const app = express();
+app.use(express.json());
+
+// File Write operation
+app.post("/write_file", (req, res) => {
+  const date = moment().format("MMM Do YY");
+  const dateArr = date.split(" ");
+  const time = moment().format("LTS");
+  const timeArr = time.split(/(?::| )+/);
+  const currentDate = String(new Date());
+
+  fs.writeFileSync(
+    `${currentFolder}/Files/${dateArr[0]}${dateArr[1]}${dateArr[2]}-${timeArr[0]}.${timeArr[1]}.${timeArr[2]}.${timeArr[3]}.txt`,
+    currentDate
+  );
+  res.json({
+    message: "File is written successfully",
+  });
+});
+
+//Read operation
+app.get("/get_all_files", (req, res) => {
+  let listOfFiles = [];
+  fs.readdirSync(`${currentFolder}/Files`).forEach((file) => {
+    listOfFiles.push(file);
+  });
+  if (listOfFiles.length === 0) {
+    res.send("No files found");
+  } else {
+    res.send(listOfFiles);
+  }
+});
+
+//Server
+
+app.get("/", (req, res) => {
+  res.send("File Write and Read Opeartions using node js");
+});
+app.listen(3000, () => {
+  console.log("server is running");
+});
